cb71ab8741920e0baae3d8e31db2bad769586c41
Jonathan Lifflander <jliffla@sandia.gov>
2018-06-12 10:36:07 -0700

    add support for other compilers

--- build-detector.sh
+++ build-detector.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-cmake ../detector \
-      -DCMAKE_INSTALL_PREFIX=../detector-install \
-      -DCMAKE_CXX_COMPILER=clang++-mp-3.9 \
-      -DCMAKE_C_COMPILER=clang-mp-3.9 \
+compiler_c=clang-mp-3.9
+compiler_cxx=clang++-mp-3.9
+
+if test $# -gt 0
+then
+    if test $1 = "gnu"
+    then
+        compiler_c=gcc
+        compiler_cxx=g++
+    fi
+fi
+
+cmake ../detector                                  \
+      -DCMAKE_INSTALL_PREFIX=../detector-install   \
+      -DCMAKE_CXX_COMPILER=${compiler_cxx}         \
+      -DCMAKE_C_COMPILER=${compiler_c}             \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=true
