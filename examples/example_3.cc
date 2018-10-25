9d6cceb3806644e597a88199c7b8d946baf4d09c
Jonathan Lifflander <jliffla@sandia.gov>
2018-10-25 16:41:53 -0700

    add test case for positional index detection in constructor

--- examples/example_3.cc
+++ examples/example_3.cc
@@ -43,6 +43,45 @@ struct Fail3 {
   void myTest(int, double);
 };
 
+struct Index1D {};
+
+struct TestConstructorNoIdx {
+  TestConstructorNoIdx(int32_t, int32_t);
+};
+
+struct TestConstructorIdxFst {
+  TestConstructorIdxFst(Index1D idx, int32_t, int32_t);
+};
+
+struct TestConstructorIdxSnd {
+  TestConstructorIdxSnd(int32_t, int32_t, Index1D idx);
+};
+
+template <typename ColT, typename IndexT, typename... Args>
+struct DetectCons {
+  template <typename U>
+  using non_idx_t = decltype(U(std::declval<Args>()...));
+  template <typename U>
+  using idx_fst_t = decltype(U(std::declval<IndexT>(),std::declval<Args>()...));
+  template <typename U>
+  using idx_snd_t = decltype(U(std::declval<Args>()...,std::declval<IndexT>()));
+
+  using has_non_index_cons = detection::is_detected<non_idx_t, ColT>;
+  using has_index_fst      = detection::is_detected<idx_fst_t, ColT>;
+  using has_index_snd      = detection::is_detected<idx_snd_t, ColT>;
+
+  static constexpr auto const non_index_cons = has_non_index_cons::value;
+  static constexpr auto const index_fst      = has_index_fst::value;
+  static constexpr auto const index_snd      = has_index_snd::value;
+
+  static constexpr auto const use_no_index   =
+    non_index_cons && !index_snd && !index_fst;
+  static constexpr auto const use_index_fst  =
+    index_fst;
+  static constexpr auto const use_index_snd  =
+    index_snd && !index_fst;
+};
+
 }} // end namespace detector::example
 
 int main(int, char**) {
@@ -58,6 +97,28 @@ int main(int, char**) {
   static_assert(TestTrait<Fail1>::followsTestTraitConvert, "Should follow.");
   static_assert(TestTrait<Fail2>::followsTestTraitConvert, "Should follow.");
   static_assert(not TestTrait<Fail3>::followsTestTraitConvert, "Should not follow.");
+
+  static_assert(
+    DetectCons<TestConstructorNoIdx, Index1D, int32_t, int32_t>::non_index_cons &&
+    !DetectCons<TestConstructorNoIdx, Index1D, int32_t, int32_t>::index_fst &&
+    !DetectCons<TestConstructorNoIdx, Index1D, int32_t, int32_t>::index_snd,
+    "No index detector failed"
+  );
+
+  static_assert(
+    !DetectCons<TestConstructorIdxFst, Index1D, int32_t, int32_t>::non_index_cons &&
+    DetectCons<TestConstructorIdxFst, Index1D, int32_t, int32_t>::index_fst &&
+    !DetectCons<TestConstructorIdxFst, Index1D, int32_t, int32_t>::index_snd,
+    "First index detector failed"
+  );
+
+  static_assert(
+    !DetectCons<TestConstructorIdxSnd, Index1D, int32_t, int32_t>::non_index_cons &&
+    !DetectCons<TestConstructorIdxSnd, Index1D, int32_t, int32_t>::index_fst &&
+    DetectCons<TestConstructorIdxSnd, Index1D, int32_t, int32_t>::index_snd,
+    "Second index detector failed"
+  );
+
 
   return 0;
 }
